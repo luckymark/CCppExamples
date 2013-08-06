2bc397ac389bcdffe484cd2869776c4a217a215b
Mark Feng <luckyzhigang@gmail.com>
2013-08-07 02:28:11 +0800

    第一个示例：HelloWorld

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,12 @@
+// helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	cout<<"Hello world!"<<endl<<"I'm Mark."<<endl;
+	return 0;
+}
+
