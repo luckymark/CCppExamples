257e337fd1eda7f066eafa6c9ebd6986a5d23002
Mark <luckyzhigang@mail.com>
2013-09-01 19:07:39 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,16 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'src/img',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'slides/img'                  // Destination path prefix
+                }]
+            }
+        },
         watch: {
             less: {
                 files: ["src/less/**/*.less"],
@@ -36,11 +46,17 @@ module.exports = function(grunt) {
             jade: {
                 files: ["src/jade/**/*.jade"],
                 tasks: ["jade:compile"]
+            },
+            imagemin: {
+                files: ["src/img/**/*.{png,jpg,gif}"],
+                tasks: ["imagemin:dynamic"]
             }
         }
     });
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-jade");
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks("grunt-contrib-watch");
+
     return grunt.registerTask("default", ["less:compile", "jade:compile"]);
 };
